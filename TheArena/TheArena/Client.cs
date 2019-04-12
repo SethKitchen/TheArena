240315fe0cd60d8f2ac411e6812ce52251095ff5
Seth Kitchen <sjkyv5@mst.edu>
2019-04-12 18:02:25 -0500

    updated old

--- TheArena/TheArena/Client.cs
+++ TheArena/TheArena/Client.cs
@@ -1,10 +1,12 @@
 ﻿using Logger;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -125,8 +127,7 @@ namespace TheArena
                                     Log.TraceMessage(Log.Nav.NavOut, status + " " + winReason + " " + loseReason + " " + logURL + " " + winnerName + " " + winnerSubmissionNumber + " " + loserName + " " + loserSubmissionNumber, Log.LogType.Info);
                                     HTTP.HTTPPostSendToWeb(status, winReason, loseReason, logURL, winnerName, winnerSubmissionNumber, loserName, loserSubmissionNumber);
                                     resultStr = winnerName + ";" + logURL;
-				    Thread.Sleep(8000);, Log.LogType.Info
-				    Directory.Delete(ARENA_FILES_PATH, true);
+                                    Directory.Delete(ARENA_FILES_PATH, true);
                                     Directory.CreateDirectory(ARENA_FILES_PATH);
                                 }
                             }
@@ -140,7 +141,7 @@ namespace TheArena
             }
         }
 
-        public static void RunGame(object fileo, ref List<string> toReturn)
+        public static void RunGame(object fileo, string gameSession, ref List<string> toReturn)
         {
             try
             {
@@ -156,32 +157,32 @@ namespace TheArena
                 if (file.ToLower().Contains("js"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building javascript ", Log.LogType.Info);
-                    toReturn.Add(Javascript.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.js/main.js",DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.js/main.js", DA_GAME, gameSession));
                 }
                 else if (file.ToLower().Contains("cpp"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building Cpp ", Log.LogType.Info);
-                    toReturn.Add(Cpp.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.cpp/main.cpp", DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.cpp/main.cpp", DA_GAME, gameSession));
                 }
                 else if (file.ToLower().Contains("py"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building Python ", Log.LogType.Info);
-                    toReturn.Add(Python.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.py/main.py", DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.py/main.py", DA_GAME, gameSession));
                 }
                 else if (file.ToLower().Contains("lua"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building Lua ", Log.LogType.Info);
-                    toReturn.Add(Lua.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.lua/main.lua",DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.lua/main.lua", DA_GAME, gameSession));
                 }
                 else if (file.ToLower().Contains("java"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building Java ", Log.LogType.Info);
-                    toReturn.Add(Java.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.java/main.java",DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.java/main.java", DA_GAME, gameSession));
                 }
                 else if (file.ToLower().Contains("cs"))
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Building Csharp ", Log.LogType.Info);
-                    toReturn.Add(CSharp.BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.cs/main.cs",DA_GAME));
+                    toReturn.Add(BuildAndRun(file.Substring(0, file.LastIndexOf(".")) + "/Joueur.cs/main.cs", DA_GAME, gameSession));
                 }
             }
             catch (Exception ex)
@@ -190,11 +191,91 @@ namespace TheArena
             }
         }
 
+        /// <summary>
+        /// Given the file path, compile the AI and run it using C++ -- run until the results file shows a win, loss, or error
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static string BuildAndRun(string file, string DA_GAME, string gameSession)
+        {
+
+            bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+            if (isWindows)
+            {
+                Log.TraceMessage(Log.Nav.NavIn, "Is Windows...", Log.LogType.Info);
+                Log.TraceMessage(Log.Nav.NavIn, "Starting Background Process...", Log.LogType.Info);
+                //Unimplemented
+            }
+            else if (isLinux)
+            {
+                Log.TraceMessage(Log.Nav.NavIn, "Is Linux.", Log.LogType.Info);
+                using (Process process = new Process())
+                {
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardError = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = false;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.FileName = Environment.GetEnvironmentVariable("SHELL");
+                    Log.TraceMessage(Log.Nav.NavIn, "Grabbing Shell Process...", Log.LogType.Info);
+                    if (process.Start())
+                    {
+                        process.StandardInput.WriteLine("cd " + file.Substring(0, file.LastIndexOf('/')));
+
+                        if (File.Exists(file.Substring(0, file.LastIndexOf('/') + 1) + "testRun"))
+                        {
+                            File.Delete(file.Substring(0, file.LastIndexOf('/') + 1) + "testRun");
+                        }
+                        using (StreamWriter sw = new StreamWriter(file.Substring(0, file.LastIndexOf('/') + 1) + "testRun"))
+                        {
+                            sw.AutoFlush = true;
+                            sw.WriteLine("#!/bin/bash");
+                            sw.WriteLine("if [ -z \"$1\" ]");
+                            sw.WriteLine("  then");
+                            sw.WriteLine("    echo \"No argument(s) supplied. Please specify game session you want to join or make.\"");
+                            sw.WriteLine("  else");
+                            sw.WriteLine("    ./run " + DA_GAME + " -s 127.0.0.1 -r \"$@\"");
+                            sw.WriteLine("fi");
+                        }
+                        Log.TraceMessage(Log.Nav.NavIn, "Rewrote script-- running", Log.LogType.Info);
+                        process.StandardInput.WriteLine("sudo chmod 777 testRun && sudo chmod 777 run && sudo make clean && sudo make && ./testRun " + gameSession + " >>results.txt 2>&1");
+                        string result = "";
+                        do
+                        {
+                            Log.TraceMessage(Log.Nav.NavIn, "Results file not done waiting 1 min...", Log.LogType.Info);
+                            Thread.Sleep(1000 * 60); //Wait 1 min for game to finish
+                            string resultsFile = file.Substring(0, file.LastIndexOf('/') + 1) + "results.txt";
+                            Log.TraceMessage(Log.Nav.NavIn, "Results File=" + resultsFile, Log.LogType.Info);
+                            if (File.Exists(resultsFile))
+                            {
+                                Log.TraceMessage(Log.Nav.NavIn, "Results file exists reading...", Log.LogType.Info);
+                                using (StreamReader sr = new StreamReader(resultsFile))
+                                {
+                                    result = sr.ReadToEnd() + Environment.NewLine + file;
+                                }
+                                Log.TraceMessage(Log.Nav.NavIn, "Results=" + result, Log.LogType.Info);
+                            }
+                            else
+                            {
+                                Log.TraceMessage(Log.Nav.NavIn, "Results file does not exist...", Log.LogType.Info);
+                            }
+                        } while (!result.ToLower().Contains("won") && !result.ToLower().Contains("lost") && !(result.ToLower().Contains(" error ") && !result.ToLower().Contains("0 error")));
+                        Log.TraceMessage(Log.Nav.NavIn, "Results contains win or lose or com error--returning...", Log.LogType.Info);
+                        return result;
+                    }
+                }
+            }
+            return "";
+        }
+
+
         static List<string> BuildAndRunGame()
         {
             try
             {
                 List<string> answers = new List<string>();
+                string gameSession = "seth" + DateTime.Now.Ticks;
                 Log.TraceMessage(Log.Nav.NavIn, "Building and Running Game ", Log.LogType.Info);
                 if (!Directory.Exists(ARENA_FILES_PATH))
                 {
@@ -206,7 +287,7 @@ namespace TheArena
                 foreach (var file in files)
                 {
                     Log.TraceMessage(Log.Nav.NavIn, "Creating Thread for file " + file, Log.LogType.Info);
-                    allGames.Add(Task.Run(() => RunGame(file, ref answers)));
+                    allGames.Add(Task.Run(() => RunGame(file, gameSession, ref answers)));
                 }
                 Log.TraceMessage(Log.Nav.NavIn, "Starting WaitAll ", Log.LogType.Info);
                 Task.WaitAll(allGames.ToArray());
--- TheArena/TheArena/HTTP.cs
+++ TheArena/TheArena/HTTP.cs
@@ -9,6 +9,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Web;
 
 namespace TheArena
 {
@@ -18,19 +19,41 @@ namespace TheArena
         public string session;
         public gameSettings gameSettings;
 
-        /*public string status;
+        public string status;
         public string winReason;
         public string loseReason;
         public string logUrl;
         public Winner winner;
-        public Loser loser;*/
+        public Loser loser;
     }
 
     public class gameSettings
     {
         public string[] playerNames;
     }
-        
+
+    public class Client
+    {
+        public int index;
+        public string name;
+        public bool spectating;
+        public bool disconnected;
+        public bool timedOut;
+        public bool won;
+        public bool lost;
+        public string reason;
+    }
+
+    public class Result
+    {
+        public string gameName;
+        public string gameSession;
+        public string gamelogFilename;
+        public string status;
+        public int numberOfPlayers;
+        public List<Client> clients = new List<Client>();
+    }
+
 
     public class Winner
     {
@@ -46,9 +69,9 @@ namespace TheArena
 
     public class HTTP
     {
-        public static void HTTPPostStartGame(string gameName, string session, gameSettings gameSettings, string[] playerNames)
+        public static bool HTTPPostStartGame(string gameName, string session, gameSettings gameSettings)
         {
-            MyPacket p = new MyPacket() { gameName = gameName, session = session, gameSettings = gameSettings};
+            MyPacket p = new MyPacket() { gameName = gameName, session = session, gameSettings = gameSettings };
             string serialized = JsonConvert.SerializeObject(p);
             Log.TraceMessage(Log.Nav.NavIn, "Serialized data: " + serialized, Log.LogType.Info);
             using (var client = new HttpClient())
@@ -56,28 +79,80 @@ namespace TheArena
                 try
                 {
                     List<Task> allGames = new List<Task>();
-                    Console.WriteLine("Wat");
                     //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IklMaWtlU29ja3NPblN1bmRheXMiLCJpZCI6Mywicm9sZSI6InVzZXIiLCJpYXQiOjE1NDE4NjY1NTcsImV4cCI6MTU0MjI5ODU1N30.XWaWB_cWhUFEC1m0GxFJ4ln8uq5h086gXGxRmOLVXA0");
-                    allGames.Add(Task.Run(async () => {var x = await client.PostAsync(
-                        "http://35.190.137.139:3080/setup", new StringContent(serialized, Encoding.UTF8, "application/json"));
-                         
-                         Console.WriteLine(await x.Content.ReadAsStringAsync());
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + await x.Content.ReadAsStringAsync(), Log.LogType.Info);
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
-                         }));
+                    allGames.Add(Task.Run(async () => {
+                        var x = await client.PostAsync(
+"http://127.0.0.1:3080/setup",
+new StringContent(serialized, Encoding.UTF8, "application/json"));
+                        Console.WriteLine(await x.Content.ReadAsStringAsync());
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + await x.Content.ReadAsStringAsync(), Log.LogType.Info);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
+                    }));
+                    Task.WaitAll(allGames.ToArray());
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    string idk = ex.Message;
+                    Log.TraceMessage(Log.Nav.NavIn, "Except: " + idk, Log.LogType.Info);
+                    return false;
+                }
+            }
+        }
+
+        public static Result HTTPPostGetStatus(string gameName, string gameSession)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    List<Task> allGames = new List<Task>();
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNldGhBZG1pbiIsImlkIjo0LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1NTUwMjU0NDQsImV4cCI6MTU1NTQ1NzQ0NH0.m4bOU5k5hH2YTnDT0094oDA1XDHxsQqMxNkSQQFCaHE");
+                    //build url
+                    string url = "http://127.0.0.1:3080/status/" + gameName + "/" + gameSession;
+                    allGames.Add(Task.Run(async () => {
+                        var x = await client.GetAsync(url);
+                        string response = await x.Content.ReadAsStringAsync();
+                        Console.WriteLine(response);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + response, Log.LogType.Info);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
+                        Result deserialized;
+
+                        try
+                        {
+                            deserialized = JsonConvert.DeserializeObject<Result>(response);
+                            Console.WriteLine(deserialized.clients);
+                        }
+                        catch (Exception k)
+                        {
+                            string except = k.Message;
+                            Log.TraceMessage(Log.Nav.NavIn, "Except: " + k, Log.LogType.Info);
+                            Console.WriteLine("Exception. Check Log.");
+                            return null;
+                        }
+
+                        //to remove warnings on compile...
+                        string trash = deserialized.gameSession;
+                        string trash1 = deserialized.gameName;
+                        int trash2 = deserialized.numberOfPlayers;
+
+                        return deserialized;
+                    }));
                     Task.WaitAll(allGames.ToArray());
                 }
                 catch (Exception ex)
                 {
                     string idk = ex.Message;
                     Log.TraceMessage(Log.Nav.NavIn, "Except: " + idk, Log.LogType.Info);
+                    return null;
                 }
             }
+            return null;
         }
 
         public static void HTTPPostSendToWeb(string status, string winReason, string loseReason, string logURL, string winnerTeamName, string winnerVersion, string loserTeamName, string loserVersion)
         {
-            /*MyPacket p = new MyPacket() { status = status, loseReason = loseReason, winReason = winReason, logUrl = logURL, winner=new Winner() { teamName = winnerTeamName, version = winnerVersion },loser=new Loser() { teamName = loserTeamName, version = loserVersion } };
+            MyPacket p = new MyPacket() { status = status, loseReason = loseReason, winReason = winReason, logUrl = logURL, winner = new Winner() { teamName = winnerTeamName, version = winnerVersion }, loser = new Loser() { teamName = loserTeamName, version = loserVersion } };
             string serialized = JsonConvert.SerializeObject(p);
             Console.WriteLine(serialized);
             using (var client = new HttpClient())
@@ -85,7 +160,7 @@ namespace TheArena
                 try
                 {
                     List<Task> allGames = new List<Task>();
-                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IklMaWtlU29ja3NPblN1bmRheXMiLCJpZCI6Mywicm9sZSI6InVzZXIiLCJpYXQiOjE1NDE4NjY1NTcsImV4cCI6MTU0MjI5ODU1N30.XWaWB_cWhUFEC1m0GxFJ4ln8uq5h086gXGxRmOLVXA0");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNldGhBZG1pbiIsImlkIjo0LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1NTUwMjU0NDQsImV4cCI6MTU1NTQ1NzQ0NH0.m4bOU5k5hH2YTnDT0094oDA1XDHxsQqMxNkSQQFCaHE");
                     allGames.Add(Task.Run(() => client.PostAsync(
                         "https://mmai-server.siggame.io/games/",
                          new StringContent(serialized, Encoding.UTF8, "application/json"))));
@@ -95,7 +170,7 @@ namespace TheArena
                 {
                     string idk = ex.Message;
                 }
-            }*/
+            }
         }
 
     }
